3eca97be8e0bb0fecdafb887524a27385c787374
FakeDoctor22 <giljoelvillarbaatwork@gmail.com>
2023-06-29 20:22:23 +0800

    commit c

--- app.js
+++ app.js
@@ -17,7 +17,10 @@ const connectToDB = async (url) => {
 
 connectToDB(config.MONGODB_URI);
 
-app.use(cors());
+app.use(cors({
+    origin: 'http://localhost:5173',
+    credentials: true
+  }));
 app.use(express.json());
 app.use(express.static("dist"));
 app.use("/api/users", userRouter);
